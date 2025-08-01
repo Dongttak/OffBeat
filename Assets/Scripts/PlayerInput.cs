a8c63973bfeca8da3e1df162d2b053a77327663c
nanen123 <lkjhgf13579@naver.com>
2025-08-01 18:29:10 +0900

    플레이어 이동시 총 보이도록 조정

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,7 +9,9 @@ public class PlayerInput : MonoBehaviour
     private int posIndex = 1; // 현재 위치 인덱스 0(왼쪽), 1(가운데), 2(오른쪽)
     public List<Transform> positions; // 라인(플레이어가 이동할 위치) 담을 리스트
     public static event Action AttackEvent; // 공격시 발동하는 이벤트
-    
+    public GameObject gun;
+    public List<Transform> gunPositions;
+
     void Update()
     { 
         // 왼쪽 이동
@@ -20,6 +22,11 @@ public class PlayerInput : MonoBehaviour
                 transform.DOMove(positions[--posIndex].position, 0.2f).SetEase(Ease.InOutQuad); // DOTween 에셋 내장 함수
                        // DOMove(목표 위치, 이동까지 걸리는 시간).SetEase(...)
                 Debug.Log("Move Left");
+                if (posIndex == 1)
+                {
+                    gun.transform.position = gunPositions[1].position;
+                }
+
             }
         }
 
@@ -29,7 +36,11 @@ public class PlayerInput : MonoBehaviour
             if (posIndex < 2 && BeatManager.Instance.IsOnBeatNow())
             {
                 transform.DOMove(positions[++posIndex].position, 0.2f).SetEase(Ease.InOutQuad);
-                Debug.Log("Move Right");
+                Debug.Log("Move Right"); 
+                if (posIndex == 2)
+                {
+                    gun.transform.position = gunPositions[0].position;
+                }
             }
         }
 
