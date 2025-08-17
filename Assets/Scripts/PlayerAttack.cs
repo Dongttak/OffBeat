43b698379218dec97330936087f3372823fd9ae3
Dongttak <109863563+Dongttak@users.noreply.github.com>
2025-08-17 22:32:12 +0900

    Update PlayerAttack.cs

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,45 +1,50 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private RaycastHit hit;
-    public Transform firePosition; // 총구 위치
-    private bool attackRequired = false; // 공격이 입력되었는지 확인하는 변수
-    
-    void Awake()
-    {
-        PlayerInput.AttackEvent += () => attackRequired = true;
-    }
+    [Header("Raycast")]
+    [SerializeField] private Transform firePosition;
+    [SerializeField] private float range = 30f;
+    [SerializeField] private LayerMask hitMask = ~0;
+    [SerializeField] private QueryTriggerInteraction triggerInteraction = QueryTriggerInteraction.Ignore;
 
-    private void LateUpdate()
-    {
-        if (attackRequired)
-        {
-            Attack();
-            attackRequired = false;
-        }
-    }
+    [Header("Hit Feedback (optional)")]
+    [SerializeField] private string enemyTag = "Enemy";
+    [SerializeField] private bool flashOnHit = true;
+    [SerializeField] private Color flashColor = Color.red;
+    [SerializeField] private float flashTime = 0.08f;
 
-    void OnDrawGizmos()
+    public void Attack()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(firePosition.position, transform.forward * 30);
-    }
-    private void Attack()
-    {
-        if (Physics.Raycast(firePosition.position, Vector3.forward, out hit, 30f))
+        Vector3 origin = firePosition ? firePosition.position : transform.position;
+        Vector3 dir = firePosition ? firePosition.forward : transform.forward;
+
+        if (Physics.Raycast(origin, dir, out RaycastHit hit, range, hitMask, triggerInteraction))
         {
-            if (hit.transform.CompareTag("Enemy"))
+            Debug.DrawLine(origin, hit.point, Color.yellow, 0.25f);
+
+            if (hit.transform.CompareTag(enemyTag))
             {
-                hit.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+                var rend = hit.transform.GetComponent<Renderer>();
+                if (flashOnHit && rend && rend.material && rend.material.HasProperty("_Color"))
+                    StartCoroutine(Flash(rend));
+
+           
             }
         }
         else
         {
-            Debug.Log("fail");
+            Debug.DrawRay(origin, dir * range, Color.cyan, 0.25f);
         }
+    }
+
+    private System.Collections.IEnumerator Flash(Renderer rend)
+    {
+        var mat = rend.material;
+        if (!mat.HasProperty("_Color")) yield break;
+        Color original = mat.color;
+        mat.color = flashColor;
+        yield return new WaitForSeconds(flashTime);
+        mat.color = original;
     }
 }
